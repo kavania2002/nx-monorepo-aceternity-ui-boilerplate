d22c5f5c66a0caa7a29c87a2b57c305ddb79e9db
Kavan Gondalia <kavan155gondalia@gmail.com>
2024-10-05 17:14:44 +0530

    feat(lib): add ui-components library

---
+++ libs/ui-components/src/lib/ui-components.tsx
@@ -0,0 +1,11 @@
+import styles from './ui-components.module.css';
+
+export function UiComponents() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to UiComponents!</h1>
+    </div>
+  );
+}
+
+export default UiComponents;
